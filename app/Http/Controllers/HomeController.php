e79d5591ceb62a287d5e9eb3df9ee27538f188bf
Visavesj <desper011@hotmail.co.th>
2020-04-23 18:50:36 +0700

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \Pusher\Pusher;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function authenticate(Request $request) {
+        $socketId = $request->socket_id;
+        $channelName = $request->channel_name;
+
+        $pusher = new Pusher('f3f4f9a07461797c0392', '1007950a53c5e6757012', '981894', [
+            'cluster' => 'ap1',
+            'useTLS' => true
+        ]);
+
+        $presence_data = ['name' => auth()->user()->name];
+        $key = $pusher->presence_auth($channelName, $socketId, auth()->id(), $presence_data);
+
+        return response($key);
+    }
+}
